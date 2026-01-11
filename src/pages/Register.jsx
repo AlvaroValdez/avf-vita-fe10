e8dbbc35102e56c550f863e68702ef9812035991
Alvaro Valdez <v.alvaro.r@gmail.com>
2026-01-11 15:15:29 -0300

    change sendgrid to godaddy email

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,6 +8,8 @@ const Register = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [error, setError] = useState('');
   const [success, setSuccess] = useState(''); // Estado para el mensaje de éxito
   const [loading, setLoading] = useState(false);
